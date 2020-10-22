dd6969d94a29cf5cd1f83ad02568c5845d753cd0
Lidija Šuster <lilly.sh@hotmail.com>
2020-10-22 16:21:02 +0200

    Add Ingredients to Recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,28 @@
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'Test', 'https://i.pinimg.com/originals/8d/c5/31/8dc531fd1168a51c9e5bc9aca0d45051.jpg'),
-        new Recipe('Another Test Recipe', 'Test', 'https://i.pinimg.com/originals/8d/c5/31/8dc531fd1168a51c9e5bc9aca0d45051.jpg')
+        new Recipe(
+            'Pizza',
+            'Extra hot sauce pizza',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTfpeXZhCIXBZa4OubuouGNip1XjsC49GfPXw&usqp=CAU',
+            [
+                new Ingredient('Mushrooms', 20),
+                new Ingredient('Olives', 10),
+                new Ingredient('Hot sauce', 1)
+            ]),
+        new Recipe(
+            'Kobasice',
+            'Super hot barbecue sausages',
+            'https://p0.pikrepo.com/preview/409/766/pile-of-grilled-sausages.jpg',
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('French Fries', 30)
+            ])
     ];
 
     getRecipes() {
