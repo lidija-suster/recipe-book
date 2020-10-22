4297a1b7eab51e8a75945065e150a0c1bc2b8da1
Lidija Šuster <lilly.sh@hotmail.com>
2020-10-22 16:01:11 +0200

    Manage Shopping List and Push Data from A to B

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,23 @@
-export class ShoppingListService {
+import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
+export class ShoppingListService {
+    ingretientsChanged = new EventEmitter<Ingredient[]>();
+
+    private ingredients: Ingredient[] = [
+        new Ingredient('Apples', 5),
+        new Ingredient('Tomatoes', 10),
+    ];
+
+    getIngredients() {
+        return this.ingredients.slice();
+    }
+
+    addIngredient(ingredient: Ingredient) {
+        // It's not working because we are pushing new ingredient to original array, 
+        // but getting copy of an array (with slice method)
+        this.ingredients.push(ingredient);
+        // Emit event to a copy of an array
+        this.ingretientsChanged.emit(this.ingredients.slice());
+    }
 }
