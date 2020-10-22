5fdee8503349da454e8d86e2ecab43ce7d8e5609
Lidija Šuster <lilly.sh@hotmail.com>
2020-10-22 14:34:45 +0200

    Manage Recipes in Recipe Service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,12 @@
-export class RecipeService {
+import { Recipe } from './recipe.model';
 
+export class RecipeService {
+    private recipes: Recipe[] = [
+        new Recipe('A Test Recipe', 'Test', 'https://i.pinimg.com/originals/8d/c5/31/8dc531fd1168a51c9e5bc9aca0d45051.jpg'),
+        new Recipe('Another Test Recipe', 'Test', 'https://i.pinimg.com/originals/8d/c5/31/8dc531fd1168a51c9e5bc9aca0d45051.jpg')
+    ];
+
+    getRecipes() {
+        return this.recipes.slice();
+    }
 }
