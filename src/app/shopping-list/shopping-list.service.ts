0b6d9b81c93c3e4eacec2395beb8501f7cd7be68
Lidija Šuster <lilly.sh@hotmail.com>
2020-11-04 11:36:37 +0100

    Add Delete Functionality

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -38,4 +38,9 @@ export class ShoppingListService {
         this.ingredients[index] = newIngredient;
         this.ingretientsChanged.next(this.ingredients.slice());
     }
+
+    deleteIngredient(index: number) {
+        this.ingredients.splice(index, 1);
+        this.ingretientsChanged.next(this.ingredients.slice());
+    }
 }
