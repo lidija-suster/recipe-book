08886a72ea56b5512e5bb0651f5ede7255bfa7da
Lidija Šuster <lilly.sh@hotmail.com>
2020-11-12 20:18:11 +0100

    Add Logic for Storing Recipes

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,9 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { RecipeService } from '../recipes/recipe.service';
 
 @Injectable({
     providedIn: 'root'
 })
 export class DataStorageService {
-    constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient, private recipeService: RecipeService) { }
+
+    storeRecipes() {
+        const recipes = this.recipeService.getRecipes();
+        return this.http.put('https://recipe-book-830ab.firebaseio.com/recipes.json', recipes)
+            .subscribe(response => {
+                console.log(response);
+            });
+    }
 }
