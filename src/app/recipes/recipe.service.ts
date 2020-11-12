870150c944c4db5c4f6c40f2dc0fc8cf37808af5
Lidija Šuster <lilly.sh@hotmail.com>
2020-11-12 20:45:49 +0100

    Transform Response Data

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,25 +8,27 @@ import { Recipe } from './recipe.model';
 export class RecipeService {
     recipesChanged = new Subject<Recipe[]>();
 
-    private recipes: Recipe[] = [
-        new Recipe(
-            'Pizza',
-            'Extra hot sauce pizza',
-            'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTfpeXZhCIXBZa4OubuouGNip1XjsC49GfPXw&usqp=CAU',
-            [
-                new Ingredient('Mushrooms', 20),
-                new Ingredient('Olives', 10),
-                new Ingredient('Hot sauce', 1)
-            ]),
-        new Recipe(
-            'Kobasice',
-            'Super hot barbecue sausages',
-            'https://p0.pikrepo.com/preview/409/766/pile-of-grilled-sausages.jpg',
-            [
-                new Ingredient('Meat', 10),
-                new Ingredient('French Fries', 30)
-            ])
-    ];
+    // private recipes: Recipe[] = [
+    //     new Recipe(
+    //         'Pizza',
+    //         'Extra hot sauce pizza',
+    //         'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTfpeXZhCIXBZa4OubuouGNip1XjsC49GfPXw&usqp=CAU',
+    //         [
+    //             new Ingredient('Mushrooms', 20),
+    //             new Ingredient('Olives', 10),
+    //             new Ingredient('Hot sauce', 1)
+    //         ]),
+    //     new Recipe(
+    //         'Kobasice',
+    //         'Super hot barbecue sausages',
+    //         'https://p0.pikrepo.com/preview/409/766/pile-of-grilled-sausages.jpg',
+    //         [
+    //             new Ingredient('Meat', 10),
+    //             new Ingredient('French Fries', 30)
+    //         ])
+    // ];
+
+    private recipes: Recipe[] = [];
 
     constructor(private shoppingListService: ShoppingListService) { }
 
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
+import { map } from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root'
@@ -19,6 +20,11 @@ export class DataStorageService {
 
     fetchRecipes() {
         this.http.get<Recipe[]>('https://recipe-book-830ab.firebaseio.com/recipes.json')
+            .pipe(map(recipes => {
+                return recipes.map(recipe => {
+                    return { ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] }
+                });
+            }))
             .subscribe(recipes => {
                 this.recipeService.setRecipes(recipes);
             });
