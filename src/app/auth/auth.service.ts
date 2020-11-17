e2d68433bda6473636a51f65850ad41242be26d6
Lidija Šuster <lilly.sh@hotmail.com>
2020-11-17 20:28:55 +0100

    Add Spiner and Error Handling

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 interface AuthResponseData {
     kind: string;
@@ -23,6 +25,16 @@ export class AuthService {
                 email: email,
                 password: password,
                 returnSecureToken: true
-            });
+            }).pipe(catchError(errorRes => {
+                let errorMessage = 'An unknown error occurred!';
+                if (!errorRes.error || !errorRes.error.error) {
+                    return throwError(errorMessage);
+                }
+                switch (errorRes.error.error.message) {
+                    case 'EMAIL_EXISTS':
+                        errorMessage = 'This email already exists';
+                }
+                return throwError(errorMessage);
+            }));
     }
 }
